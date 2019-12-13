19b74e255065c30ef9a42d7474e341a8e3e0d61d
Machine-Hum <ryan.cjw@gmail.com>
2019-12-13 14:34:59 -0800

    Added sprites

---
+++ resources/sprits/characters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="characters" tilewidth="16" tileheight="16" tilecount="96" columns="12">
+ <image source="characters.png" width="192" height="128"/>
+</tileset>
