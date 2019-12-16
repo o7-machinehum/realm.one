fa8a0b6502bf97d968b5b45993e54ea9ad340c81
Machine-Hum <ryan.cjw@gmail.com>
2019-12-16 10:37:03 -0800

    Sprites rendering, built a python script for making .ron files

---
+++ resources/sprites/ronMaker.py
@@ -0,0 +1,38 @@
+#eg: python3 ronMaker.sh <fname.ron> <texture_width> <texture_height> <width> <height>
+
+import sys
+
+fname              = sys.argv[1]  
+texture_width      = int(sys.argv[2]) 
+texture_height     = int(sys.argv[3])
+width              = int(sys.argv[4])
+height             = int(sys.argv[5])
+cols               = int(texture_width   / width)
+rows               = int(texture_height  / height)
+
+f = open(fname, "w+")
+
+f.write('(\n')
+f.write('texture_width: ' + str(texture_width) +  ',\ntexture_height: ' + str(texture_height) + ',\nsprites: [', )
+
+x = 0
+y = 0
+
+for ii in range(0, rows):
+    for i in range(0, cols):
+        f.write('\n(');
+        f.write('\nx: ' + str(x))
+        f.write(',\ny: ' + str(y))
+        f.write(',\nwidth: ' + str(width))
+        f.write(',\nheight: ' + str(height))
+        if x == (texture_width - width)  and y == (texture_height - height): 
+            f.write(',\n)');
+        else: 
+            f.write(',\n),');
+       
+        x += width
+    y += height
+    x = 0
+
+f.write('\n]')
+f.write('\n)')
