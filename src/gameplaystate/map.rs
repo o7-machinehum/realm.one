230692f025b57c5a63c8bb239396e2657f3d3366
Machine-Hum <ryan.cjw@gmail.com>
2019-12-18 15:40:17 -0800

    Serious refector

--- src/gameplaystate/map.rs
+++ src/gameplaystate/map.rs
@@ -1,3 +1,11 @@
+use amethyst::{
+    assets::{AssetStorage, Loader},
+    core::transform::Transform,
+    input::{get_key, is_close_requested, is_key_down, VirtualKeyCode}, prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+    window::ScreenDimensions,
+};
+
 extern crate tiled;
 
 use std::fs::File;
@@ -8,9 +16,9 @@ use log::info;
 use tiled::parse;
 
 pub struct Room {
-    name: String,
-    pub current: tiled::Map,
+    pub current: tiled::Map,   // Current room 
     pub tiles: Vec<i32>,
+    pub sprites: Vec<SpriteRender>,
 }
 
 impl Room {
@@ -32,9 +40,75 @@ impl Room {
         info!("{:?}", mp);
 
         Self{
-            name: fname,
             tiles: Room::countTiles(&mp), 
             current: mp,
+            sprites: Vec::new(), 
+        }
+    }
+    
+    pub fn load_sprites(&mut self, world: &mut World) {
+        let mut ii = 0;
+    
+        for sets in &self.current.tilesets {
+            // Load the texture for our sprites. We'll later need to
+            // add a handle to this texture to our `SpriteRender`s, so
+            // we need to keep a reference to it.
+            let texture_handle = {
+                let loader = world.read_resource::<Loader>();
+                let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+                loader.load(
+                    // "sprites/basictiles.png",
+                    format!("sprites/{}.png", sets.name), 
+                    ImageFormat::default(),
+                    (),
+                    &texture_storage,
+                )
+            };
+    
+            // Load the spritesheet definition file, which contains metadata on our
+            // spritesheet texture.
+            let sheet_handle = {
+                let loader = world.read_resource::<Loader>();
+                let sheet_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
+                loader.load(
+                    // "sprites/basictiles.ron",
+                    format!("sprites/{}.ron", sets.name), 
+                    SpriteSheetFormat(texture_handle),
+                    (),
+                    &sheet_storage,
+                )
+            };
+            
+           for i in (0..self.tiles[ii]){ 
+               self.sprites.push(SpriteRender{
+                   sprite_sheet: sheet_handle.clone(),
+                   sprite_number: i as usize,
+               });
+           };
+           ii += 1;
+        };
+    }
+    
+    pub fn load_room(&mut self, world: &mut World, dimensions: &ScreenDimensions) {
+        let mut x = 0.0;
+        let mut y = 0.0;
+
+        for row in self.current.layers[0].tiles.iter().rev(){
+            x = 0.0;
+            y += 16.0;
+            for col in row.iter(){
+                x += 16.0; 
+                let mut transform = Transform::default();
+                transform.set_translation_xyz(x, y, 0.);
+                
+                if(*col != 0){
+                    world
+                        .create_entity()
+                        .with(self.sprites[*col as usize - 1].clone())
+                        .with(transform)
+                        .build();
+                }
+            }
         }
     }
 }
