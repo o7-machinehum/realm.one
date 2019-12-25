e2bd39921a965200c00c8d9a2c140db87f78c89b
Louis Vinchon <louis.vinchon.dev@gmail.com>
2019-12-25 16:24:52 -0600

    Add key bindings enums
    This will allow us to use enum variants to designate key bindings
    instead of strings which are error-prone since the compiler can't check
    them, unlike enum variants.

--- src/systems/player.rs
+++ src/systems/player.rs
@@ -1,9 +1,13 @@
 use amethyst::core::{Transform, SystemDesc};
 use amethyst::derive::SystemDesc;
 use amethyst::ecs::{Join, Read, ReadStorage, System, SystemData, World, WriteStorage};
-use amethyst::input::{InputHandler, StringBindings};
+use amethyst::input::InputHandler;
+
 use crate::state::{Player};
+use crate::key_bindings::{MovementBindingTypes, AxisBinding, ActionBinding};
 use log::info;
+
+const TILE_SIZE : f32 = 16.0;
 
 #[derive(SystemDesc)]
 pub struct PlayerSystem;
@@ -12,18 +16,20 @@ impl<'s> System<'s> for PlayerSystem{
     type SystemData = (
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Player>,
-        Read<'s, InputHandler<StringBindings>>,
+        Read<'s, InputHandler<MovementBindingTypes>>,
     );
 
     fn run(&mut self, (mut transforms, players, input): Self::SystemData) {
-        for (player, transform) in (&players, &mut transforms).join(){  
-            let xMove = input.action_is_down("up").unwrap_or(false);
+        for (player, transform) in (&players, &mut transforms).join() {  
+            let horizontal = input
+                .axis_value(&AxisBinding::Horizontal)
+                .unwrap_or(0.0);
+            let vertical = input
+                .axis_value(&AxisBinding::Vertical)
+                .unwrap_or(0.0);
 
-            if xMove {
-                info!("UP");  
-                // let xMove = 1.2 * mv_amount as f32;
-                transform.move_up(16.0);     
-            }
+            transform.move_up(vertical * TILE_SIZE);
+            transform.move_right(horizontal * TILE_SIZE);
         } 
     }
 }
