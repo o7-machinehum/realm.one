7b2076a02a050bf96d0f6393adc948ae48665b21
turnip <machinehum@wlkr.wlker>
2020-01-08 12:35:13 -0800

    Broke main into two parts

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,53 @@ use amethyst::{
     },
     input::InputBundle,
     utils::application_root_dir,
+    network::NetworkBundle,
 };
+
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::env; 
+use log::info;
 
 mod map;
 mod key_bindings;
-
 mod states;
 mod components;
 mod systems;
 mod constants;
 mod mech;
-
 mod character_sprites;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
-
+    let args: Vec<String> = env::args().collect();
+    let mut rtn : amethyst::Result<()> = Ok(()); 
     let app_root = application_root_dir()?;
     let resources = app_root.join("resources");
+    
+    if args[1] == "client" {
+        info!("Starting the client");
+        rtn = client(resources);
+    }
+
+    else if args[1] == "server"{
+        info!("Starting the server!");
+        rtn = server(resources);
+    }
+    // else error out
+    
+    rtn
+}
+
+fn client(resources: std::path::PathBuf) -> amethyst::Result<()> {
     let display_config = resources.join("display_config.ron");
     let key_bindings_config_path = resources.join("bindings.ron");
     
     let input_bundle = InputBundle::<key_bindings::MovementBindingTypes>::new()
         .with_bindings_from_file(key_bindings_config_path)?;
-
+    
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let network_bundle = NetworkBundle::<String>::new(socket);
+     
     let game_data = GameDataBuilder::default()
         .with_bundle(TransformBundle::new())?
         .with_bundle(
@@ -43,6 +66,7 @@ fn main() -> amethyst::Result<()> {
                 .with_plugin(RenderFlat2D::default()),
         )?
         .with_bundle(input_bundle)? 
+        .with_bundle(network_bundle)? 
         .with(systems::PlayerSystem, "player_system", &["input_system"]);
 
 
@@ -53,7 +77,22 @@ fn main() -> amethyst::Result<()> {
     )?;
 
     game.run();
-
     Ok(())
 }
 
+fn server(resources: std::path::PathBuf) -> amethyst::Result<()> {
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let network_bundle = NetworkBundle::<String>::new(socket);
+     
+    let game_data = GameDataBuilder::default()
+        .with_bundle(network_bundle)?; 
+
+    let mut game = Application::new(
+        resources, 
+        states::GamePlayState{},
+        game_data,
+    )?;
+
+    game.run();
+    Ok(())
+}
