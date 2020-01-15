23f426681be9ceea1a87e0eb46a38a6ee120d7ad
turnip <machinehum@wlkr.wlker>
2020-01-15 14:16:05 -0800

    Map rendering over UDP WORKING!

--- src/systems/map.rs
+++ src/systems/map.rs
@@ -1,6 +1,6 @@
 use amethyst::core::{Transform, SystemDesc};
 use amethyst::derive::SystemDesc;
-use amethyst::ecs::{Join, Read, Entities, System, SystemData, World, WriteStorage};
+use amethyst::ecs::{Join, Read, Write, Entities, System, SystemData, World, WriteStorage, Entity};
 use amethyst::renderer::SpriteRender;
 
 use crate::map::{Room, TilePosition, SpritesContainer};
@@ -14,7 +14,7 @@ impl<'s> System<'s> for MapSystem{
         WriteStorage<'s, Transform>,
         WriteStorage<'s, SpriteRender>,
         WriteStorage<'s, TilePosition>,
-        Read<'s, Room>,
+        Write<'s, Room>,
         Read<'s, SpritesContainer>,
         Entities<'s>,
     );
@@ -22,21 +22,32 @@ impl<'s> System<'s> for MapSystem{
     /// Should ONLY be called in a re-draw event of the map
     /// Resource room should be updated with the newest room
     fn run(&mut self, (mut transforms, mut sprite_renders, mut tiles_pos, mut room, mut container, entities): Self::SystemData) {
-        for (sprite_render, transform, pos) in (&mut sprite_renders, &mut transforms, &mut tiles_pos).join() { 
-            match room.diff_gid(pos) {
-                Some(gid) => {
-                    pos.gid = gid;                                           // Change the gid in the position 
-                    if gid == 0 {
-                        //TODO: This is just inserting a blank sprites into the spriterender
-                        // I don't know if this is the best way to do this.
-                        *sprite_render = container.sprites[49].clone();      
-                    }
-                    else {
-                        *sprite_render = container.sprites[gid - 1].clone(); // Change the sprite
-                    }
-                },
-                None => {},
+        if room.update {
+            // Delete old tiles
+            for entity in room.tile_ent.iter() {
+                entities.delete(*entity);
             }
+            
+            let mut entList: Vec<Entity> = Vec::new();
+            for (z, layer) in room.map.layers.iter().enumerate() {
+                for (x, row) in layer.tiles.iter().rev().enumerate() {
+                    for (y, col) in row.iter().enumerate() {
+                        if col.gid != 0 {
+                            let mut loc = TilePosition::new(x, y, z, col.gid as usize - 1);
+                            let mut transform = loc.to_trans(); 
+                            entList.push(
+                                entities.build_entity()
+                                    .with(container.sprites[loc.gid].clone(), &mut sprite_renders) 
+                                    .with(transform, &mut transforms)
+                                    .with(loc, &mut tiles_pos) 
+                                    .build()
+                            );
+                        }
+                    }
+                }
+            }
+            room.update = false;
+            room.tile_ent.append(&mut entList); 
         }
     }
 }
