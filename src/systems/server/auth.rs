03d46a74a9fad92aa5723c29d309ba3fe7ae82bd
Machine-Hum <ryan.cjw@gmail.com>
2020-01-21 10:36:30 -0800

    Server now had uid's for clients

--- src/systems/server/auth.rs
+++ src/systems/server/auth.rs
@@ -14,21 +14,26 @@ use crate::network;
 use log::info;
 use crate::network::{Pack, Cmd, IO};
 use crate::components::{PlayerList, PlayerAction, PlayerInfo, Action};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 /// A simple system that receives a ton of network events.
 #[derive(SystemDesc)]
 pub struct AuthSystem;
 
-fn welcome(proof: String) -> Pack {
+// fn generate_id(proof: String) -> Pack {
+// 
+// }
+
+fn insert_map(proof: String, ip: Option<SocketAddr>) -> Pack {
     info!("Player Connected proof: {}, sending map!", proof);
     let fname = "resources/maps/townCompress2.tmx";
     let mut file = File::open(&fname.to_string()).expect("Unable to open map file"); 
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Failed to convert to string");
-    Pack::new(Cmd::TransferMap(fname.to_string(), contents.to_string()), 0)
+    Pack::new(Cmd::TransferMap(fname.to_string(), contents.to_string()), 0, ip)
 }
 
-fn ready_player_one() -> Pack {
+fn ready_player_one(ip: Option<SocketAddr>) -> Pack {
     info!("Inserting player 1");
    
     // This should be loaded in the future
@@ -46,7 +51,7 @@ fn ready_player_one() -> Pack {
         we: 294, 
     };
 
-    Pack::new(Cmd::InsertPlayer(player1_info), 0)
+    Pack::new(Cmd::InsertPlayer(player1_info), 0, ip)
 }
 
 impl<'a> System<'a> for AuthSystem {
@@ -58,11 +63,11 @@ impl<'a> System<'a> for AuthSystem {
         for element in io.0.I.pop() {
             match &element.cmd {
                 Cmd::Connect(packet) => {
-                    io.0.O.push(welcome(packet.to_string())); 
-                    io.0.O.push(ready_player_one());
+                    io.0.O.push(insert_map(packet.to_string(), element.ip())); 
+                    io.0.O.push(ready_player_one(element.ip()));
                 },
                 _ => (io.0.I.push(element)), 
             }
         }
-    } 
+    }
 }
