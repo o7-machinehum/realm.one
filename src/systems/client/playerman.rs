1cd5774ded231cfaab26de725d16159a4abe3797
turnip <machinehum@wlkr.wlker>
2020-01-30 12:15:05 -0800

    WIP

--- src/systems/client/playerman.rs
+++ src/systems/client/playerman.rs
@@ -1,6 +1,6 @@
 use amethyst::core::{Transform};
 use amethyst::derive::SystemDesc;
-use amethyst::ecs::{Read, Write, System, SystemData, WriteStorage, Join};
+use amethyst::ecs::{Read, Write, System, SystemData, WriteStorage, Join, Entities};
 use amethyst::renderer::SpriteRender;
 use log::info;
 
@@ -20,9 +20,10 @@ impl<'s> System<'s> for PlayerManSystem{
         WriteStorage<'s, SpriteRender>,
         Write<'s, IO>,
         Read<'s, SpritesContainer>,
+        Entities<'s>,
     );
  
-    fn run(&mut self, (mut transforms, mut players, mut sprite_renders, mut io, _s): Self::SystemData) {
+    fn run(&mut self, (mut transforms, mut players, mut sprite_renders, mut io, _s, entities): Self::SystemData) {
         for element in io.i.pop() {
             match element.cmd {
                 Cmd::UpdatePlayer(new) => {
@@ -41,9 +42,13 @@ impl<'s> System<'s> for PlayerManSystem{
                         }
                     }        
                 }, 
-                Cmd::RemovePlayer(_ip) => {
-                    // Remove the player and delete the
-                    // components and entities
+                Cmd::RemovePlayer(ip) => {
+                    info!("Removing Player of ip: {}", ip);
+                    for (e, player) in (&*entities, &mut players).join() { 
+                        if player.ip == ip {
+                            entities.delete(e).expect("Failed to delete old player entities");
+                        }
+                    } 
                 }, 
                 _ => io.i.push(element), 
             }
