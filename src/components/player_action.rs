607f593f5faea805b2401a596ea413b4f72e24a8
machine-hum <machinehum@wlkr.wlker>
2020-02-05 10:39:44 -0800

    Integrated outfit changing, not tested!

--- src/components/player_action.rs
+++ src/components/player_action.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use crate::components::{Orientation};
+use crate::components::{Skins};
 
 #[warn(dead_code)]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -8,4 +9,5 @@ pub enum Action {
     Move(Orientation),
     Attack(u32),
     UseItem(u32),
+    ChangeOutfit(Skins),
 }
