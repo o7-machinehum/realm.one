fc01d676a66d76070113a61e30e9d138c889fd78
machine-hum <machinehum@wlkr.wlker>
2020-02-05 10:24:48 -0800

    WIP

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,6 +1,8 @@
 mod player;
 pub use self::player::PlayerComponent;
 pub use self::player::Orientation;
+pub use self::player::Skins;
+pub use self::player::get_outfit;
 
 mod player_action;
 pub use self::player_action::Action;
