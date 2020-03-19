05b807b4834d900bb292793513f33d9ee968439c
machine-hum <ryan.cjw@gmail.com>
2020-03-19 14:55:51 -0700

    Player can visually melee now

--- src/systems/client/melee.rs
+++ src/systems/client/melee.rs
@@ -1,8 +1,9 @@
 use amethyst::{
-    core:: {Time, Transform},
+    core:: {Time, Transform, Parent},
     ecs::{Read, System, WriteStorage, ReadStorage, Join, Entities, Entity},
     renderer::SpriteRender
 };
+use log::info;
 
 use crate::{
     components::{MeleeAnimation, PlayerComponent},
@@ -30,36 +31,42 @@ impl<'s> System<'s> for MeleeAnimationSystem {
         WriteStorage<'s, Transform>,
         WriteStorage<'s, SpriteRender>,
         WriteStorage<'s, MeleeAnimation>,
+        WriteStorage<'s, Parent>,
         Entities<'s>, 
         Read<'s, Time>,
         Read<'s, SpritesContainer>,
     );
 
-    fn run(&mut self, (mut transforms, mut sprite_renders, mut anims, entities, time, s): Self::SystemData) {
+    fn run(&mut self, (mut transforms, mut sprite_renders, mut anims, mut parent, entities, time, s): Self::SystemData) {
         for item in self.delete_list.pop() {
-            anims.remove(item);
+            anims.remove(item);  // Remove the animation component
         }
-        
-        for (sprite_render, anim, e) in (&mut sprite_renders, &mut anims, &entities).join() {
+
+        for (anim, e) in (&mut anims, &entities).join() {
             anim.update(time.delta_seconds());
 
             match anim.get_seq() {
                 Some(new_spr) => {
-                    sprite_render.sprite_number = new_spr;
+                    if let Some(sprite_render) = sprite_renders.get_mut(e) {
+                        sprite_render.sprite_number = new_spr;
+                    }
                 }
                 None => ()
             };
 
             if anim.delete() {
+                // Delete the sword entity
+                entities.delete(anim.sword.unwrap().clone());
                 self.delete_list.push(e.clone());
             }
 
-            if anim.draw_sword == true {
-                entities
-                    .build_entity()
-                    .with(s.sprites[anim.sword].clone(), &mut sprite_renders)
-                    .with(anim.sword_pos, &mut transforms) 
-                    .build();
+            if anim.sword.is_none() {
+                anim.sword = Some(entities
+                .build_entity()
+                .with(s.sprites[anim.sword_spr].clone(), &mut sprite_renders)
+                .with(anim.sword_pos.clone(), &mut transforms) 
+                .with(Parent::new(e), &mut parent) 
+                .build()); 
             }
         }
     }
