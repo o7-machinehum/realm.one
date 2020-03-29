b48759bdfa6565d514ed3a259191b23244de4e0a
machine-hum <ryan.cjw@gmail.com>
2020-03-29 10:55:01 -0700

    ip field on playercomponent is now Option<SocketAddr>. As it will be none for monsters.

--- src/systems/server/auth.rs
+++ src/systems/server/auth.rs
@@ -37,7 +37,7 @@ fn ready_player_one(ip: Option<SocketAddr>, name: String, id: u64) -> LifeformCo
     info!("Inserting player 1 ({})", name);
    
     // Dig through database to find the correct player by name = name 
-    LifeformComponent::new(name, ip.unwrap(), id)
+    LifeformComponent::new_player(name, ip.unwrap(), id)
 }
 
 impl<'a> System<'a> for AuthSystem {
@@ -56,13 +56,13 @@ impl<'a> System<'a> for AuthSystem {
                         Some(s) => {
                             let player = ready_player_one(element.ip(), s, id.add());
 
-                            io.o.push(Pack::new(Cmd::TransferMap(player.room.clone()), Dest::Ip(player.ip))); 
+                            io.o.push(Pack::new(Cmd::TransferMap(player.room.clone()), Dest::Ip(player.ip()))); 
                             io.o.push(Pack::new(Cmd::InsertPlayer1(player.clone()), Dest::All));
                             
                             // Push the rest of the players
                             for p in pl.list.iter() {
                                 match p {
-                                    Some(p) => io.o.push(Pack::new(Cmd::InsertPlayer(p.clone()), Dest::Ip(player.ip))),
+                                    Some(p) => io.o.push(Pack::new(Cmd::InsertPlayer(p.clone()), Dest::Ip(player.ip()))),
                                     None => (),
                                 }
                             }
