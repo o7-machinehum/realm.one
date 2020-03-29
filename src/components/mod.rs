de32434fa58ef942e9756d4d8c5cb3829327ac12
machine-hum <ryan.cjw@gmail.com>
2020-03-28 22:49:18 -0700

    Working on pulling out monsters from the map file

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -3,6 +3,10 @@ pub use self::lifeform::LifeformComponent;
 pub use self::lifeform::LifeformType;
 pub use self::lifeform::Orientation;
 
+mod monster; 
+pub use self::monster::Monster;
+
+
 mod player_action;
 pub use self::player_action::Action;
 
