234903f709084f81b0688984161bcc60164d8c8e
machine-hum <ryan.cjw@gmail.com>
2020-04-05 19:51:41 -0700

    WIP. Broke somethings going to revert.

--- src/systems/server/network.rs
+++ src/systems/server/network.rs
@@ -32,22 +32,30 @@ pub struct TcpSystemDesc;
 impl<'a, 'b> SystemDesc<'a, 'b, TcpSystem> for TcpSystemDesc {
     fn build(self, world: &mut World) -> TcpSystem {
         <TcpSystem as System<'_>>::SystemData::setup(world);
-        let reader = world
+        let net_reader = world
             .fetch_mut::<EventChannel<NetworkSimulationEvent>>()
             .register_reader();
-        TcpSystem::new(reader)
+        
+        
+        let event_reader = world
+            .fetch_mut::<EventChannel<Pack>>()
+            .register_reader();
+
+        TcpSystem::new(net_reader, event_reader)
     }
 }
 
 pub struct TcpSystem {
-    reader: ReaderId<NetworkSimulationEvent>,
+    net_reader: ReaderId<NetworkSimulationEvent>,
+    event_reader: ReaderId<Pack>,
     clients: Vec<SocketAddr>,
 }
 
 impl TcpSystem {
-    pub fn new(reader: ReaderId<NetworkSimulationEvent>) -> Self {
+    pub fn new(net_reader: ReaderId<NetworkSimulationEvent>, event_reader: ReaderId<Pack>) -> Self {
         Self { 
-            reader,
+            net_reader,
+            event_reader,
             clients: Vec::<SocketAddr>::new(),
         }
     }
@@ -55,6 +63,7 @@ impl TcpSystem {
 
 impl<'a> System<'a> for TcpSystem {
     type SystemData = (
+        Read<'a, EventChannel<Pack>>,
         Write<'a, EventChannel<AuthEvent>>,
         Write<'a, TransportResource>,
         Read<'a, NetworkSimulationTime>,
@@ -63,10 +72,10 @@ impl<'a> System<'a> for TcpSystem {
         Write<'a, LifeformList>,
     );
 
-    fn run(&mut self, (mut auth, mut net, sim_time, channel, mut io, mut pl): Self::SystemData) {
+    fn run(&mut self, (in_packs, mut auth, mut net, sim_time, channel, mut io, mut pl): Self::SystemData) {
         let mut packs = Vec::<Pack>::new();
         // First we get the Events
-        for event in channel.read(&mut self.reader) {
+        for event in channel.read(&mut self.net_reader) {
             match event {
                 NetworkSimulationEvent::Message(addr, payload) => {
                     info!("Package: {:?}", payload);
@@ -127,6 +136,33 @@ impl<'a> System<'a> for TcpSystem {
                             info!("Sending pack: {:?} to: {:?}", resp, ip);
                             net.send(ip, &resp.to_bin());
                         }
+                    }
+                }
+            }
+        }
+
+        // This is the new way!
+        for pack in in_packs.read(&mut self.event_reader) {
+            match &pack.dest {
+                // Just send to one address 
+                Dest::Ip(addr) => {
+                    info!("Sending pack: {:?} to: {:?}", pack, addr);
+                    net.send(*addr, &pack.to_bin());
+                },
+                // Broadcast message
+                Dest::All => {
+                    info!("Broadcasting pack: {:?}", pack);
+                    for addr in &self.clients {
+                        info!("Sending pack: {:?} to: {:?}", pack, addr);
+                        net.send(*addr, &pack.to_bin());
+                    }
+                },
+                Dest::Room(name) => {
+                    // Get all the ip's in the room
+                    let ips = pl.ip_in_room(&name);
+                    for ip in ips { 
+                        info!("Sending pack: {:?} to: {:?}", pack, ip);
+                        net.send(ip, &pack.to_bin());
                     }
                 }
             }
