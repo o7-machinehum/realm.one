6233a9333a54f030cc8e4c070c4a1f91ee2ef55f
Machine-Hum <ryan.cjw@gmail.com>
2020-06-10 19:58:22 -0700

    Items coming up and being inserted as entities

--- src/systems/client/wallet.rs
+++ src/systems/client/wallet.rs
@@ -105,73 +105,44 @@ impl<'s> System<'s> for WalletSystem {
 fn listen_client(tx_main: mpsc::Sender<Item>) {
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
     info!("Server listening on port 3333");
-    // match listener.accept() {
-    let mut rx_list = Vec::<mpsc::Receiver<Item>>::new();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                info!("New connection: {}", stream.peer_addr().unwrap());
-                let (tx, rx) = mpsc::channel();
-                rx_list.push(rx);
+    match listener.accept() {
+        Ok((stream, addr)) => {
+            info!("New connection: {}", addr); 
 
-                thread::spawn(move|| {
-                    // connection succeeded
-                    handle_client(stream, tx)
-                });
-            },
-            Err(e) => {
-                info!("{:?}", e);
-                /* connection failed */
-            }
-        }
-        for rx in &rx_list {
-            match rx.try_recv() {
-                Ok(item) => {
-                    info!("{:?}", item);
-                    tx_main.send(item).unwrap();
-                },
-                Err(_e) => (),
-            }
+            thread::spawn(move|| {
+                // connection succeeded
+                handle_client(stream, tx_main)
+            });
+        },
+        Err(e) => {
+            info!("{:?}", e);
+            /* connection failed */
         }
     }
-}
 
-fn as_u32_be(array: &[u8; 4]) -> u32 {
-    ((array[0] as u32) << 24) +
-    ((array[1] as u32) << 16) +
-    ((array[2] as u32) <<  8) +
-    ((array[3] as u32) <<  0)
 }
 
 fn handle_client(mut stream: TcpStream, tx: mpsc::Sender<Item>) {
     let mut data = [0 as u8; 4098]; // buffer size 
     let mut ptr: usize = 0;
-    let mut pack_size: usize = 0;
+    let mut string_size: usize;
 
     while match stream.read(&mut data) {
         Ok(size) => {
-            info!("Size: {}", size);
             while ptr < size {
-                // First four bytes are the size of the pack 
-                if pack_size == 0 {
-                    // pack_size = as_u32_be(&data[0..4]) as usize;
-                    pack_size = usize::from_be_bytes(data[0..4].try_into().unwrap());
-                    info!("Pack Size: {}", pack_size);
-                    ptr += 4;
-                    if size < pack_size {
-                        info!("Shit, this shouldn't have happend. We got half a pack");
-                    }
-                    else {
-                        let msg = from_utf8(&data[ptr..ptr+pack_size]).unwrap().to_string(); 
-                        info!("{:?}", msg);
-                        let item = Item::new(msg);
-                        tx.send(item).unwrap();
-                        ptr += pack_size;
-                        info!("Ptr: {}", ptr);
-                        pack_size = 0;
-                    }
+                string_size = u32::from_le_bytes(data[ptr..ptr+4].try_into().unwrap()) as usize;
+                ptr += 4;
+
+                if size < string_size {
+                    info!("Shit, this shouldn't have happend. We got half a pack");
                 }
-                // info!("{:?}", item);
+                else {
+                    let msg = from_utf8(&data[ptr..ptr+string_size]).unwrap().to_string(); 
+                    let item = Item::new(msg);
+                    // info!("Item: {:?}", item);
+                    tx.send(item).unwrap();
+                    ptr += string_size;
+                }
             }
             true
         },
